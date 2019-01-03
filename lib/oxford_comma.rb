8ea78726997c294afa8392488222175fe09ef81f
Luke Granger <grangerl330@gmail.com>
2019-01-03 18:06:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,6 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length == 3
     array[-1] = "and #{array[-1]}"
-    array.join(", ")
-    return
+    return array.join(", ")
   end
 end
