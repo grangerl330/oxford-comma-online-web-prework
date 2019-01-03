f84ab2b5d22a68021b5f0ee52d7b4a86368d5798
Luke Granger <grangerl330@gmail.com>
2019-01-03 18:07:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2 
     array.join(" and ")
-  elsif array.length == 3
+  elsif array.length  3
     array[-1] = "and #{array[-1]}"
-    return array.join(", ")
+    array.join(", ")
   end
 end
